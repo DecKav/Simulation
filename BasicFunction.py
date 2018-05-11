798ba5587ad8363f0624ba37e393399f55e7c920
Declan Kavanagh <dec.kavanagh16@gmail.com>
2018-05-11 10:25:38 +1000

    Added time step removal and car progression beginnings

--- BasicFunction.py
+++ BasicFunction.py
@@ -27,7 +27,7 @@ class Node:
     
     def addCar(self, Car, time):
         self.queue[Car] = time
-        print(Car, "added to", self.name+".")
+        print(Car.ID, "added to", self.name+".")
     
     def removeCar(self, Car):
         del self.queue[Car]
@@ -63,9 +63,16 @@ class Charger(Node):
         Node.__init__(self, name, x, y)
 
 class Task:
+    """ Represents a warehouse job.
+    Attributes:
+        Node list
+        Time at each stage
     
-    def __init__():
-        pass
+    """
+    def __init__(self, ID, node1, node2):
+        self.ID = ID
+        self.start = node1
+        self.finish = node2
       
 class Link:
     """ Represents a link between nodes that cars will travel along. 
@@ -97,12 +104,21 @@ class Car:
         Current Charge
         Current Location/Direction
         Current State 
+        
+    State(int):
+            0 = Charging
+            1 = Idling
+            2 = Moving
+            3 = At goods/picking node
     """
     def __init__(self, ID):
         self.ID = ID
         self.state = 0
+        self.task = None
     
-    
+    def addTask(self, task):
+        self.task = task
+        self.state = 1
 
 class Simulator:
     """ Controls all nodes, links, and cars. Advances time, manages job list.  
@@ -110,47 +126,81 @@ class Simulator:
     Attributes:
         Node list
         Link list
-        Car list    
+        Car list
+        Task list
     """
     def __init__(self):
         self.nodes = {}
         self.cars = {}
         self.links = {}
+        self.tasks = {}
     
     def addNode(self, name, x, y):
         self.nodes[name] = Node(name,x,y)
         print(name, "created.")
     
     def addCar(self, ID):
-        self.cars[ID] = Car(ID)
-        print(ID, "created.")
+        carAdd = Car(ID)
+        self.cars[carAdd.ID] = carAdd
+        print(carAdd.ID, "created.")
         
     def addLink(self, node1, node2):
         linkAdd = Link(node1, node2)
         self.links[linkAdd.name] = linkAdd
         print(linkAdd.name, "created.")
+        
+    def addTask(self, ID, node1, node2):
+        taskAdd = Task(ID, node1, node2)
+        self.tasks[taskAdd.ID] = taskAdd
+        print("Number", taskAdd.ID, "job created.")
     
     def time_step(self):
-        print("Stepping time.")
+        linkRemove = []
+        nodeRemove = []
         
+        #Increments cars in nodes
         for node in self.nodes:
             nodeRemove = []
             for car in self.nodes[node].queue:
                 self.nodes[node].queue[car] = self.nodes[node].queue[car] - 1
+                #Check for cars finishing
                 if self.nodes[node].queue[car] == 0:
                     nodeRemove = nodeRemove.append(car)
-                    print(car, "finished at", node+".")
-            #for car in nodeRemove:
-                #node.removeCar(car)
-                    
+                    print(car.ID, "finished at", node+".")
+            #Remove finished cars
+            if nodeRemove:
+                for car in nodeRemove:
+                    print(car.ID, "removed from", node+".")
+                    node.removeCar(car)
+                    car.state = 2
+        
+        #Increment cars in links            
         for link in self.links:
             for car in self.links[link].queue:
+                linkRemove = []
                 self.links[link].queue[car] = self.links[link].queue[car] - 1
+                #Check for cars finishing
                 if self.links[link].queue[car] == 0:
-                    #self.links[link].removeCar(car)
+                    linkRemove = linkRemove.append(car)
                     print(car.ID, "finished travelling along", link+".")
- 
-                   
+            #Remove finished cars
+            if linkRemove:
+                for car in linkRemove:
+                    link.removeCar(car)
+                    car.state = 3
+        
+       #Move cars onto next state in task             
+        for car in sim.cars:
+            if self.cars[car].state == 2:
+                #Progress from node to link
+                sim.links["AB"].addCar(car,6)
+                self.cars[car].state = 1
+                
+            if self.cars[car].state == 3:
+                #Progress from link to node
+                #Dependent on path assigned
+                sim.nodes[self.cars[car].task.finish.name].addCar(car,6)
+                self.cars[car].state = 1 
 
 if __name__ == "__main__":
     sim = Simulator()
@@ -158,7 +208,23 @@ if __name__ == "__main__":
     sim.addNode("nodeB", 2, 1)
     sim.addCar("Car1")
     sim.addLink(sim.nodes["nodeA"], sim.nodes["nodeB"])
+    sim.addTask(1, sim.nodes["nodeA"], sim.nodes["nodeB"])
     
+    sim.cars["Car1"].addTask(sim.tasks[1])
+    
+    sim.nodes[sim.cars["Car1"].task.start.name].addCar(sim.cars["Car1"], 6)
+    
+    count = 0
+    
+    while(count<50):
+        
+        sim.time_step()
+        count = count + 1
+        
+                
+                
+        
+    """
     sim.nodes["nodeA"].addCar(sim.cars["Car1"].ID, 6)
     
     count = 0
@@ -178,4 +244,5 @@ if __name__ == "__main__":
     while(count<6):
         sim.time_step()
         count += 1
+    """
 
