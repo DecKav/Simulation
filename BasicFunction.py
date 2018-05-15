563bff25b83bc50447ae3d281c7af028aa266cc2
sambloom547 <38844964+sambloom547@users.noreply.github.com>
2018-05-15 15:20:17 +1000

    Added basic OrderTracker class

--- BasicFunction.py
+++ BasicFunction.py
@@ -10,6 +10,11 @@ A simulation for an intelligent warehouse, using cars, nodes, and links.
 Next: different node types, automatic car change, jobs, car attributes, charge node/battery model, payload
 Add order tracker: [#, node list, completed]
 
+Sam changes 15/5:
+    added Task duration
+    added getName method for Node to improve readability
+    Added order tracker class
+    I have a feeling we should change Node such that we add tasks, not cars, to each node.
 """
 
 import math
@@ -19,6 +24,7 @@ class Node:
     Attributes:
         Name
         Location [x,y]
+        Queue (car times)
     """
     def __init__(self, name, x, y):
         self.name = name
@@ -32,6 +38,9 @@ class Node:
     
     def removeCar(self, Car):
         del self.queue[Car]
+        
+    def getName(self):
+        return self.name
         
 class Goods(Node):
     """ Represents a node in a warehouse that cars will retrieve and store goods at. 
@@ -66,14 +75,16 @@ class Charger(Node):
 class Task:
     """ Represents a warehouse job.
     Attributes:
-        Node list
-        Time at each stage
+        Task ID
+        Start and finish nodes
+        Deadline time to be completed by
     
     """
-    def __init__(self, ID, node1, node2):
+    def __init__(self, ID, node1, node2, duration):
         self.ID = ID
         self.start = node1
         self.finish = node2
+        self.deadline = duration
       
 class Link:
     """ Represents a link between nodes that cars will travel along. 
@@ -120,6 +131,32 @@ class Car:
     def addTask(self, task):
         self.task = task
         self.state = 1
+        
+
+class OrderTracker:
+    """ A class which keeps track of the progress of all current tasks
+    Attributes:
+        Current tasks
+        Completed tasks
+        Current node of car assigned to the task
+        Task completion time
+    """
+    def __init__(self):
+        self.currentTasks = {}
+        self.completedTasks = {}
+    
+    def addNewTask(self, task):
+         # Add all new tasks to this list
+         self.currentTasks.append(task)
+         print("Task", task.ID, "added to order list")
+         
+    
+    def addCompletedTask(self, completeTask):
+        #If a task has been completed, add it to this list
+        self.completedTasks.append(completeTask)
+  
+        
+        
 
 class Simulator:
     """ Controls all nodes, links, and cars. Advances time, manages job list.  
@@ -135,6 +172,8 @@ class Simulator:
         self.cars = {}
         self.links = {}
         self.tasks = {}
+        self.orders = OrderTracker()
+        
     
     def addNode(self, name, x, y):
         nodeAdd = Node(name,x,y)
@@ -149,34 +188,42 @@ class Simulator:
     def addLink(self, node1, node2):
         linkAdd = Link(node1, node2)
         self.links[linkAdd.name] = linkAdd
-        print(linkAdd.name, "created.")
+        print("Link",linkAdd.name, "created.")
         
-    def addTask(self, ID, node1, node2):
-        taskAdd = Task(ID, node1, node2)
+    def addTask(self, ID, node1, node2, duration):
+        taskAdd = Task(ID, node1, node2, duration)
         self.tasks[taskAdd.ID] = taskAdd
-        print("Number", taskAdd.ID, "job created.")
+        print("Job number", taskAdd.ID, "created.")
+              
     
     def time_step(self):
         linkRemove = []
         nodeRemove = []
         
-        #Increments cars in nodes
+        #Increments cars in nodes, and update tasks
         for node in self.nodes:
             nodeRemove = []
             for car in self.nodes[node].queue:
-                self.nodes[node].queue[car] = self.nodes[node].queue[car] - 1
-                #Check for cars finishing
+                self.nodes[node].queue[car] = self.nodes[node].queue[car] - 1 # Decrement the queue by 1 timestep
+                
+                #Check for cars finishing when the queue time is 0
                 if self.nodes[node].queue[car] == 0:
                     nodeRemove.append(car)
                     print(car.ID, "finished at", node+".")
-
-            #Remove finished cars
+               
+                # Check for completed tasks - for each node, check if the current car's
+                # task ends at that node.
+                if car.task.finish == node:
+                    orders.addCompletedTask(car.task)
+                    print(task, "completed.")
+                                
+                #Remove finished cars
             if nodeRemove:
                 for car in nodeRemove:
                     print(car.ID, "removed from", node+".")
                     self.nodes[node].removeCar(car)
                     car.state = 2
-        
+                 
         #Increment cars in links            
         for link in self.links:
             for car in self.links[link].queue:
@@ -196,16 +243,17 @@ class Simulator:
         
        #Move cars onto next state in task             
         for car in self.cars:
-            if self.cars[car].state == 2:
+            if self.cars[car].state == 2: # If car state is 'moving'
                 #Progress from node to link
                 sim.links["AB"].addCar(self.cars[car],6)
                 self.cars[car].state = 1
                 
-            if self.cars[car].state == 3:
+            if self.cars[car].state == 3: # If car state is 'at node'
                 #Progress from link to node
                 sim.nodes[self.cars[car].task.finish.name].addCar(self.cars[car],6)
                 self.cars[car].state = 1 
-
+      
+            
 
 if __name__ == "__main__":
     sim = Simulator()
@@ -213,7 +261,7 @@ if __name__ == "__main__":
     sim.addNode("nodeB", 2, 1)
     sim.addCar("Car1")
     sim.addLink(sim.nodes["nodeA"], sim.nodes["nodeB"])
-    sim.addTask(1, sim.nodes["nodeA"], sim.nodes["nodeB"])
+    sim.addTask(1, sim.nodes["nodeA"], sim.nodes["nodeB"], 5)
     
     sim.cars["Car1"].addTask(sim.tasks[1])
     
@@ -221,7 +269,7 @@ if __name__ == "__main__":
     
     count = 0
     
-    while(count<50):
+    while(count<20):
         
         sim.time_step()
         count = count + 1
